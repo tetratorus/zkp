6921b6b6b0cc1cba84fc0202248e270635191cd1
tetratorus <tetratorus@gmail.com>
2019-09-20 02:11:24 +0800

    Add interactive commitment to a series of vectors

--- elliptic.test.js
+++ elliptic.test.js
@@ -1,4 +1,4 @@
-const { G, H, ec, random, ptsEql, Vector } = require('./utils')
+const { ec, genExpVec, G, Gvec, H, ptsEql, random, Vector } = require('./utils')
 const BN = require('bn.js')
 const fixtures = require('./fixtures.json')
 
@@ -43,5 +43,54 @@ test('Test pedersen commitments work', () => {
   const Csum = C1.add(C2)
   const check = H(1).mul(r1.add(r2)).add(ec.curve.g.mul(a1.add(a2)))
   expect(ptsEql(Csum, check)).toBe(true)
+})
+
+test('Interactive proof that we know openings of m vectors', () => {
+  let m = 2
+  let N = 3
+  let C = []
+  let r = []
+  let x = []
+  let H0 = H(0)
+  for (let i = 0; i < m; i++) {
+    let ri = random(32)
+    r.push(ri)
+    let xi = []
+    for (let j = 0; j < N; j++) {
+      xi.push(random(32))
+    }
+    let xivec = new Vector(xi)
+    x.push(xivec)
+    C.push(H0.mul(ri).add(xivec.innerProduct(Gvec(N))))
+  }
+
+  r.unshift(random(32)) // r0
+  let x0 = []
+  for (let j = 0; j < N; j++) {
+    x0.push(random(32))
+  }
+  let x0vec = new Vector(x0)
+  x.unshift(x0vec)
+  C.unshift(H0.mul(r[0]).add(x0vec.innerProduct(Gvec(N))))
+
+  let e = random(32)
+  let evec = genExpVec(e, m)
+  let z = new Vector(Array.from(Array(N)).map(() => ec.curve.point(null, null)))
+  evec.values.map(function(elem, index) {
+    z = z.ops(x[index].mul(elem), (a, b) => a.add(b))
+  })
+  let s = new BN(0)
+  evec.values.map(function(elem, index) {
+    s = s.add(elem.mul(r[index]))
+  })
+
+  let eC = ec.curve.point(null, null)
+  evec.values.map(function(elem, index) {
+    eC = eC.add(C[index].mul(elem))
+  })
+
+  let rhs = H0.mul(s).add(z.innerProduct(Gvec(N)))
+  expect(ptsEql(eC, rhs)).toBe(true)
+
 })
 
--- utils.js
+++ utils.js
@@ -3,9 +3,6 @@ const ec = new EC('secp256k1')
 const createKeccakHash = require('keccak')
 const BN = require('bn.js')
 const crypto = require('crypto')
-// const H = ecBNToPoint(new BN(createKeccakHash('keccak256')
-//   .update(ec.curve.g.x.toString('hex'))
-//   .digest('hex'), 16))
 
 const G = (function() {
   const cache = {
@@ -24,6 +21,14 @@ const G = (function() {
   return getG
 })()
 
+const Gvec = function(n) {
+  let Gs = []
+  for (let i = 1; i <= n; i++) {
+    Gs.push(G(i))
+  }
+  return new Vector(Gs)
+}
+
 const H = (function() {
   const cache = {}
   const getH = function(num) {
@@ -40,6 +45,42 @@ const H = (function() {
   }
   return getH
 })()
+
+const Hvec = function(n) {
+  let Hs = []
+  for (let i = 1; i <= n; i++) {
+    Hs.push(H(i))
+  }
+  return new Vector(Hvec)
+}
+
+const basePointGen = function(basePointSymbol) {
+  if (typeof basePointSybol !== 'string') {
+    throw new Error('basePointSymbol must be string')
+  }
+  const cache = {}
+  const getB = function(num) {
+    if (num === -1) {
+      return ec.curve.g
+    } else if (cache[num]) {
+      return cache[num]
+    } else {
+      cache[num] = ecBNToPoint(new BN(createKeccakHash('keccak256')
+        .update(getB(num - 1).x.toString('hex') + basePointSymbol)
+        .digest('hex'), 16))
+      return cache[num]
+    }
+  }
+  return getB
+}
+
+const basePointvec = function(basePoint, n) {
+  let Bs = []
+  for (let i = 1; i <= n; i++) {
+    Bs.push(basePoint(i))
+  }
+  return new Vector(Bs)
+}
 
 function ecBNToPoint(bn) {
   while (true) {
@@ -71,12 +112,12 @@ function Vector(values) {
 
 Vector.prototype.constructor = Vector
 
-Vector.prototype.combine = function(vector, operation) {
+Vector.prototype.ops = function(vector, operation) {
   if (!(vector instanceof Vector)) {
-    throw new Error('innerProduct can only be between vectors')    
+    throw new Error('ops can only be between vectors')    
   }
   if (vector.values.length !== this.values.length) {
-    throw new Error('innerProduct can only multiply two same length vectors')
+    throw new Error('ops can only multiply two same length vectors')
   }
   if (typeof operation !== 'function') {
     throw new Error('operation must be a function')
@@ -91,7 +132,7 @@ Vector.prototype.combine = function(vector, operation) {
   return new Vector(resVal)
 }
 
-Vector.prototype.apply = function(operation) {
+Vector.prototype.op = function(operation) {
   if (typeof operation !== 'function') {
     throw new Error('operation must be a function')
   }
@@ -101,6 +142,11 @@ Vector.prototype.apply = function(operation) {
   for (let i = 0; i < this.values.length; i++) {
     this.values[i] = operation(this.values[i])
   }
+  return this
+}
+
+Vector.prototype.mul = function(num) {
+  this.op(val => val.mul(num))
   return this
 }
 
@@ -116,15 +162,14 @@ Vector.prototype.innerProduct = function(vector) {
   const isBN = val => BN.isBN(val)
   for (let i = 0; i < this.values.length; i++) {
     // Cases:
-    if (isPoint(this.values[i]) && isBN(vector.values[i])) {
-    // scalars . points
+    if (isPoint(this.values[i]) && isBN(vector.values[i])) { // scalars . points
       resVals.push(this.values[i].mul(vector.values[i]))
-    } else if (isBN(this.values[i]) && isPoint(vector.values[i])) {
-    // points . scalars
+    } else if (isBN(this.values[i]) && isPoint(vector.values[i])) { // points . scalars
       resVals.push(vector.values[i].mul(this.values[i]))
-    } else if (isBN(this.values[i]) && isBN(vector.values[i])) {
-    // scalars . scalars
+    } else if (isBN(this.values[i]) && isBN(vector.values[i])) { // scalars . scalars
       resVals.push(this.values[i].mul(vector.values[i]))
+    } else {
+      throw new Error('unsupported types for innerProduct')
     }
   }
   if (isPoint(resVals[0])) {
@@ -144,13 +189,28 @@ Vector.prototype.innerProduct = function(vector) {
       }
     }, null)
   }
+}
 
+const genExpVec = function(e, m) {
+  if (!BN.isBN(e)) {
+    throw new Error('genExpVec expects BN')
+  }
+  let es = []
+  for (let i = 0; i <= m; i++) {
+    es.push(e.pow(new BN(i)))
+  }
+  return new Vector(es)
 }
 
 module.exports = {
   ec,
+  genExpVec,
   G,
+  Gvec,
   H,
+  Hvec,
+  basePointGen,
+  basePointvec,
   ptsEql,
   random,
   Vector
